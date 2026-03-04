be49efbc17196c3c229ce797b50fadbe6b7d0918
Lesego-Putswane <lesegoputswane@gmail.com>
2026-03-04 21:14:45 +0200

    added the negative number error handling

--- 01. logic_patterns/08_divisibility_patterns.py
+++ 01. logic_patterns/08_divisibility_patterns.py
@@ -22,6 +22,10 @@ while True:
     try:
         user_number = int(input("Enter a number: "))
 
+        if user_number <= 0:
+            print("Invalid input. Please enter a positive number.")
+            continue
+
         for number in range(1, user_number+1):
             divisors = []
 
