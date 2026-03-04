d782c7b006748226c5ca8af5c253f5d2c5bb5e55
Lesego-Putswane <lesegoputswane@gmail.com>
2026-03-04 20:37:21 +0200

    refactored the code to eliminate hard coded divisors, instead append them together in a list

--- 01. logic_patterns/08_divisibility_patterns.py
+++ 01. logic_patterns/08_divisibility_patterns.py
@@ -21,19 +21,16 @@ Program Requirements
 user_number = int(input("Enter a number: "))
 
 for number in range(1, user_number+1):
-    if number % 2 == 0 and number % 3 == 0 and number % 5 == 0:
-        print(f"{number} divisible by 2, 3, 5")
-    elif number % 2 == 0 and number % 3 == 0:
-        print(f"{number} divisible by 2, 3")
-    elif number % 2 == 0 and number % 5 == 0:
-        print(f"{number} divisible by 2, 5")
-    elif number % 3 == 0 and number % 5 == 0:
-        print(f"{number} divisible by 3, 5")
-    elif number % 2 == 0:
-        print(f"{number} divisible by 2")
-    elif number % 3 == 0:
-        print(f"{number} divisible by 3")
-    elif number % 5 == 0:
-        print(f"{number} divisible by 5")
+    divisors = []
+
+    if number % 2 == 0:
+        divisors.append("2")
+    if number % 3 == 0:
+        divisors.append("3")
+    if number % 5 == 0:
+        divisors.append("5")
+    
+    if divisors:
+        print(f"{number} divisible by {','.join(divisors)}")
     else:
         print(number)
